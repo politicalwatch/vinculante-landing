6300c817facf12584d00e7eb7ed73a5a28a319dd
Alex Ahumada <projectsperminute@gmail.com>
2026-09-22 11:48:40 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,52 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@vueuse/nuxt',
+    'motion-v/nuxt',
+    '@nuxt/a11y',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@dargmuesli/nuxt-cookie-control'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  content: {
+    experimental: {
+      sqliteConnector: 'native'
+    }
+  },
+
+  mdc: {
+    highlight: {
+      noApiRoute: false
+    }
+  },
+
+  compatibilityDate: '2026-06-30',
+
+  nitro: {
+    prerender: {
+      routes: [
+        '/'
+      ]
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
